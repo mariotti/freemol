3439656ac1724ef3e427f456aad7d9ae34bad214
Fabio Mariotti <fabio.mariotti@cloudfeet.net>
2015-08-16 21:26:21 +0200

    add blas and lapack source code

---
+++ Freemol/others/blas/zrotg.f
@@ -0,0 +1,24 @@
+      subroutine zrotg(ca,cb,c,s)
+cmf      complex(kind(2)) ca,cb,s
+      double complex ca,cb,s
+      double precision c
+      double precision norm,scale
+cmf      complex(kind(2)) alpha
+      double complex alpha
+cmf      external cdabs
+      if (cdabs(ca) .ne. 0.0d0) go to 10
+         c = 0.0d0
+         s = (1.0d0,0.0d0)
+         ca = cb
+         go to 20
+   10 continue
+         scale = cdabs(ca) + cdabs(cb)
+         norm = scale*dsqrt((cdabs(ca/dcmplx(scale,0.0d0)))**2 +
+     *                      (cdabs(cb/dcmplx(scale,0.0d0)))**2)
+         alpha = ca /cdabs(ca)
+         c = cdabs(ca) / norm
+         s = alpha * dconjg(cb) / norm
+         ca = alpha * norm
+   20 continue
+      return
+      end
